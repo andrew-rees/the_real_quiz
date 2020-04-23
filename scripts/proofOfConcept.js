952e58b5d778a1a1eb19caac3b2f31e97374ac96
Andrew Rees <andrewtrevor.rees@gmail.com>
2020-04-23 15:02:26 +0100

    new values in POC.js

--- scripts/proofOfConcept.js
+++ scripts/proofOfConcept.js
@@ -2,14 +2,14 @@ var SqlFunctions = require('./sqlfunctions.js');
 
 //Start Quiz Session
 
-var quizNumber = 2; //range is 2-3
-var accountId = 1;
+var quizNumber = 3; //range is 2-3
+var accountId = 3;
 
 SqlFunctions.startQuizSessionSQL(quizNumber, accountId)
 
 //Submit Edited Answer
 
-var a = 45; //range is 41-50
-var b = 'Updated for POC';
+var a = 41; //range is 41-50
+var b = 'Which type of animal is a spider';
 
 SqlFunctions.submitEditedQuestionSQL(a, b);
